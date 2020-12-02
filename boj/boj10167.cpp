678b2485f94566c3dfd06cd0f68c3dfb3c8e01d1
yuiop8747 <49551777+yuiop8747@users.noreply.github.com>
2020-12-02 10:01:02 +0900

    BOJ : 10167-금광
    Link : https://www.acmicpc.net/problem/10167
    
    분류 : 세그먼트 트리
    
    내용: N(1<=N<=3000)개의 좌표값과 해당하는 w (-1e9 <= w <= 1e9)가 주어질 때, 사각형으로
    해당 좌표들을 감싸서 얻을 수 있는 w의 최댓값을 구하는 문제
    
    풀이 : 요약하면, y start와 y end 두 선을 미리 지정해두고, 그 사이에서
    최댓값을 가지는 x start , x end를 구한다.
    
    y start를 정하는 방법은 1부터 N까지 나이브하게 보면된다.
    y end를 구하는 방법이 문제인데, 이 역시 y start + 1 부터 N까지
    나이브하게 본다면 세그먼트의 업데이트가 logN이 걸리므로
    O(N^3logN) 이 걸리게 된다.
    
    이를 방지하기 위해 y end를 특별히 설정하지 말고, y축 기준으로
    정렬한다음y start 보다 큰 y값을가진 노드들을 update 하면서 y의 값이
    바뀌기 직전인 그 순간을 y end로 둔다면, O(N^2longN) 만에 답을 구할 수
    있다.
    
    y start , y end가 지정되었으니 할 일은 y축 사이의 x축 범위를 지정하면
    된다.
    
    세그먼트 트리의 노드를 다음과 같이 정의하자.
    lsum : 현재 노드의 가장 좌측에서부터 연속한 합의 최댓값
    rsum : 현재 노드의 가장 우측에서부터 연속한 합의 최댓값
    totalsum : 현재 노드의 구간들에 포함된 모든합
    maxsum : 현재 구간에서 얻을 수 있는 최댓값
    
    L : 현재노드의 좌측 자식
    R : 현재노드의 우측 자식
    
    해당 값들을 구하는 식은 아래와 같다.
    이 부분은 직접 그려보면 쉽게 알 수 있다.
    
    lsum = max(L.lsum, R.lsum + L.totalsum)
    rsum = max(R.rsum, L.rsum + R.totalsum)
    totalsum = L.totalsum + R.totalsum
    maxsum = max({lsum, rsum, L.rsum + R.lsum, totalsum, L.maxsum, R.maxsum})
    
    maxsum 에서 L.rsum + R.lsum 을 해주는 이유는 좌,우측 둘다 붙지 않는
    경우를 고려하기 위함이다.
    
    주의할 점은 y start를 1~N 쭉 그대로 돌리지 말고, y축 값이 변경되는
    기점마다 바꿔주어야 한다.

--- boj/boj10167.cpp
+++ boj/boj10167.cpp
@@ -1,65 +1,94 @@
 #include<stdio.h>
+#include<algorithm>
+#include<iostream>
+#include<vector>
 #include<map>
+#include<math.h>
+#include<memory.h>
+#include<string>
+#include<stack>
+#include<queue>
+#include<string.h>
+#include<unordered_map>
 #include<set>
+#define mod 1000000007
+#define fio ios::sync_with_stdio(0), cin.tie(0);
 using namespace std;
-int n;
 typedef long long ll;
-map<int,int> mp;
-struct str{
-    int x,y,w;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int GO_MAX = 26;
+const int ESP = 1e-9;
+const int MAX = 100001;
+const int INF = 1e9;
+int n, m, k, t, q, arr[MAX * 3];
+struct str {
+	ll x, y, w;
 };
+str inp[3010];
 struct node {
-    int lsum=0,rsum=0;
+	ll lsum = 0, rsum = 0, totalsum = 0, maxsum = 0;
 };
-str inp[6010];
-node tr[6010*4];
-node update(int l,int r,int s,int e,int idx,int val){
-    if(r < s || e < l){
-        return tr[idx];
-    }
-    if(s <= l && r <= e) {
-        tr[idx].lsum += val;
-        return tr[idx];
-    }
-    int mid = l+r >> 1;
-    update(l,mid,s,e,idx*2,val);
-    update(mid+1,r,s,e,idx*2+1,val);
-    return tr[idx] = {tr[idx*2].lsum + tr[idx*2+1].lsum,tr[idx*2].rsum+tr[idx*2+1].rsum};
+node tr[4010 * 4];
+node update(int l, int r, int num, int idx,ll w) {
+	if (num < l || r < num) return tr[idx];
+	if (l == r) {
+		tr[idx].lsum += w;
+		tr[idx].rsum += w;
+		tr[idx].totalsum += w;
+		tr[idx].maxsum += w;
+		return tr[idx];
+	}
+	int mid = l + r >> 1;
+	node val1 = update(l, mid, num, idx * 2, w);
+	node val2 = update(mid + 1, r, num, idx * 2 + 1, w);
+	ll lsum = max(val1.totalsum + val2.lsum, val1.lsum);
+	ll rsum = max(val2.totalsum + val1.rsum, val2.rsum);
+	ll totalsum = val1.totalsum + val2.totalsum;
+	ll maxsum = max({ lsum, rsum, totalsum, val1.rsum + val2.lsum,val1.maxsum,val2.maxsum });
+	return tr[idx] = { lsum,rsum,totalsum,maxsum };
 }
-
-int find(int l,int r, int s,int e,int idx,bool v){
-    if(r < s || e < l) {
-        if(v){
-
-        }
-        else {
-            return 
-        }
-    }
-    if(s <= l && r <= e)
-        return tr[idx];
-    int mid = l+r >> 1;
-    find(l,mid,s,e,idx*2,v);
-    find(mid+1,r,s,e,idx*2+1,v);
-    if(v)
-        return tr[idx].lsum;
-}
-int main(){
-    scanf("%d",&n);
-    for(int i = 0;i<n;++i){
-        int x,y,w;
-        scanf("%d %d %d",&x,&y,&w);
-        mp[x] = 1;
-        mp[y] = 1;
-        inp[i] = {x,y,w};
-    }
-    int cnt = 1;
-    for(auto it = mp.begin(); it != mp.end(); it++){
-        it->second = cnt++;
-    }
-    for(int i = 0;i<n;++i){
-        inp[i] = {mp[inp[i].x],mp[inp[i].y]};
-    }
-
-    return 0;
+int main() {
+	scanf("%d", &n);
+	set<int> stx, sty;
+	map<int, int> mpx, mpy;
+	for (int i = 0; i < n; ++i) {
+		scanf("%lld %lld %lld", &inp[i].x, &inp[i].y, &inp[i].w);
+		stx.insert(inp[i].x);
+		sty.insert(inp[i].y);
+	}
+	int cntx = 1;
+	for (auto it = stx.begin(); it != stx.end(); ++it) {
+		mpx[*it] = cntx++;
+	}
+	int cnty = 1;
+	for (auto it = sty.begin(); it != sty.end(); ++it) {
+		mpy[*it] = cnty++;
+	}
+	for (int i = 0; i < n; ++i) {
+		inp[i] = { mpx[inp[i].x],mpy[inp[i].y],inp[i].w };
+	}
+	sort(inp, inp + n, [](str A, str B) {
+		return A.y < B.y;
+		});
+	inp[n].y = -1e9;
+	ll ans = 0;
+	for (int i = 0; i < n; ++i) {
+		bool trig = false;
+		for (int j = i; j < n; ++j) {
+			update(1, cntx, inp[j].x, 1, inp[j].w);
+			if (inp[j].y != inp[j + 1].y) {
+				if (!trig) {
+					trig = true; // y축의 기준값이 넘어가지는 부분은 처음으로 y축이 변할때이다.
+					i = j;
+				}
+				ans = max(ans, tr[1].maxsum);
+			}
+		}
+		for (int j = 0; j <= 16010; ++j) {
+			tr[j] = { 0,0,0,0 };
+		}
+	}
+	printf("%lld\n", ans);
 }
